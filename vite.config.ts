a25f17396c3fae56a8a6100667355fbacd7cca29
pikaapik <pikhaarka@gmail.com>
2025-12-12 19:16:40 +0700

    Update vite config port to 5173 and env setup

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 3000,
+    port: 5173,
     allowedHosts: ["dev-wordit.it-its.id", "wordit.it-its.id"],
   },
   preview: {
